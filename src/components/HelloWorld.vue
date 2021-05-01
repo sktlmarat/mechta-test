24c835c25be59528a97d85edd89321b70cce1a5b
Марат <marat.turlubayev@nu.edu.kz>
2021-05-01 22:43:25 +0600

    feat: partial markup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="main">
     <b-row>
-      <b-col md="2"></b-col>
-      <b-col md="4">
+      <b-col md="1"></b-col>
+      <b-col md="5">
         <div>
           <b-navbar variant="faded" type="light">
             <b-navbar-brand href="#" class="nav-brand">
@@ -13,39 +13,43 @@
         </div>
         <h1 class="main-heading mt-5">Delivery cost</h1>
         <p class="main-subheading">Enter name of the city to count delivery cost</p>
-        <input type="text" class="search my-4" placeholder="Enter name of the city">
-        <button class="search-btn">Enter</button>
-        <h4>Most popular cities</h4>
-        <table>
-          <tr>
-          <td>Nur-Sultan</td>
-          <td>Zhana Turmis</td>
-          </tr>
-          <tr>
-            <td>Almaty</td>
-            <td>Karaganda</td>
-          </tr>
-          <tr>
-            <td>Shymkent</td>
-            <td>Kentau</td>
-          </tr>
-          <tr>
-            <td>Atyrau</td>
-            <td>Aitei</td>
-          </tr>
-          <tr>
-            <td>Aktau</td>
-            <td>Pavlodar</td>
-          </tr>
-        </table>
-        <footer>
+        <div class="search-box my-4">
+          <input type="text" class="search" placeholder="Enter name of the city">
+          <button class="search-btn">Enter</button>
+        </div>
+        <h4 class="table-heading mb-4">Most popular cities</h4>
+        <div class="table-row">
+          <div class="table-col">
+            <span>Nur-Sultan</span>
+            <span>Zhana Turmis</span>
+          </div>
+          <div class="table-col">
+            <span>Almaty</span>
+            <span>Karaganda</span>
+          </div>
+          <div class="table-col">
+            <span>Shymkent</span>
+            <span>Kentau</span>
+          </div>
+          <div class="table-col">
+            <span>Atyrau</span>
+            <span>Aitei</span>
+          </div>
+          <div class="table-col">
+            <span>Aktau</span>
+            <span>Pavlodar</span>
+          </div>
+        </div>
+        <footer class="desktop-footer">
           <span>All rights reserved</span>
           <br>
           <span>Fast service 2021</span>
-          <img src="../assets/1200px-Maestro_2016.png" alt="">
-          <img src="../assets/1280px-Mastercard-logo.png" alt="">
-          <img src="../assets/logo.png" alt="">
-          <img src="../assets/visa-logo-icon-png_44632.png" alt="">
+          <div class="payment-images">
+            <img src="../assets/1200px-Maestro_2016.png" alt="">
+            <img src="../assets/1280px-Mastercard-logo.png" alt="">
+            <img src="../assets/logo.png" alt="">
+            <img src="../assets/visa-logo-icon-png_44632.png" alt="">
+          </div>
         </footer>
       </b-col>
       <b-col md="6" class="image-column">
@@ -70,79 +74,139 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap');
-html, *{
+
+html, * {
   font-family: 'Roboto', sans-serif;
 }
-.main{
-  overflow: hidden;
-  width: 100%;
-  height: 100vh;
+
+.main {
 }
-.nav-brand span{
+
+.nav-brand span {
   size: 24px;
   line-height: 40px;
   color: #283044;
   margin-left: 8px;
 }
-.main-heading{
+
+.main-heading {
   size: 48px;
   font-weight: 700;
   color: #283044;
 }
-.main-subheading{
+
+.main-subheading {
   size: 24px;
   font-weight: 400;
   color: rgba(40, 48, 68, 0.5);
 }
-.search{
+
+.search-box {
+  height: 60px;
+  overflow: hidden;
+}
+
+.search {
   background: #FFFFFF;
   border: 1px solid #E9F0EB;
-  box-sizing: border-box;
+  box-sizing: content-box;
   border-radius: 50px;
-  height: 60px;
-  width: 80%;
-  padding-left: 20px;
+  height: 90%;
+  width: 65%;
+  padding-left: 30px;
   font-weight: 700;
   font-size: 24px;
   color: #283044;
+  border-right: none;
+  padding-top: 0px;
 }
-.search:focus{
+
+.search:focus {
   outline: none;
 }
-.search::placeholder{
+
+.table-heading {
+  font-weight: 400;
+  size: 24px;
+}
+
+.search::placeholder {
   font-size: 20px;
   color: #D9E4DC;
   font-weight: normal;
 }
-.search-btn{
+
+.search-btn {
   background: linear-gradient(279.56deg, #65B3E4 15.15%, rgba(120, 161, 187, 0) 171.55%);
   border-radius: 50px;
-  width: 20%;
+  width: 25%;
+  height: 100%;
+  margin-left: -100px;
+  box-sizing: border-box;
+  font-weight: 700;
+  text-transform: uppercase;
+  size: 24px;
+  color: #FFFFFF;
+  border: none;
 }
-.image-column{
+
+.image-column {
+  overflow: hidden;
   position: relative;
   background-color: #F7F7F7;
   border-radius: 40px 0px 0px 40px;
   height: 100vh;
 }
-.car-img{
+
+.car-img {
   position: absolute;
   z-index: 10;
 }
-.road-img{
+
+.road-img {
   position: absolute;
   z-index: 7;
 }
-.peshehod{
+
+.peshehod {
   position: absolute;
   z-index: 8;
 }
-.img-title{
+
+.img-title {
   position: absolute;
   z-index: 9;
 }
-.nav-brand{
+
+.nav-brand {
   font-weight: 700;
+}
+
+.table-col span {
+  padding-left: 10px;
+  width: 40%;
+  display: inline-block;
+  font-weight: normal;
+  size: 20px;
+  color: rgba(40, 48, 68, 0.5);
+  border-bottom: 1px solid #E9F0EB;
+  margin-right: 10%;
+  padding-bottom: 5px;
+  padding-top: 10px;
+}
+
+.desktop-footer {
+  position: absolute;
+  bottom: 10px;
+}
+
+.desktop-footer {
+  size: 14px;
+  color: #283044;
+  opacity: 0.5;
+}
+.payment-images{
+  
 }
 
 </style>
